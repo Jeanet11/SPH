3f6ae714005f10b3a1780c8365844135b4860470
valentinCo <eersel-val@hotmail.com>
2017-10-09 14:49:26 +0200

    Integration du systeme graphyque du login

---
+++ pages/connexion.php
@@ -0,0 +1,25 @@
+<div class="container">
+	<div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading text-center">
+                    <h3 class="panel-title">Identification</h3>
+                </div>
+                <div class="panel-body">
+                    <form role="form">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="identifiant" name="email" type="text" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
+                            </div>
+                            <!-- Change this to a button or input when using this as a form -->
+                            <button type="button" class="btn btn-success btn-block">Login</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
