6e9307290e8b5aed6e00fca02332eca783f3dd91
Jim29800 <jbouvier1173@gmail.com>
2017-10-10 14:44:01 +0200

    merge cor

--- assets/templates/default.php
+++ assets/templates/default.php
@@ -8,13 +8,8 @@
 
     <link rel="icon" href="assets/images/favicon.ico" />
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="assets/css/header.css">
-    
-=======
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/connexion.css">
->>>>>>> 73a4a2e000de6ebddb71bc5ed51ed31db9e4b358
     <title>SPH</title>
     
 </head>
