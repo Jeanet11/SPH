b170a49dc792585fcd9893244bb6cf77586bab7f
chrisjuin <c.juin.cj@gmail.com>
2017-10-12 09:16:33 +0200

    modification

--- pages/creation_chantier.php
+++ pages/creation_chantier.php
@@ -1,49 +1,85 @@
+
+                                <!-- PARTIE HTML -->
+
+
 <div class="container">
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="span6">
-                <div class="area">
-                    <form class="form-horizontal">
-                        <div class="heading">
-                            <h4 class="form-heading"></h4>
-                        </div>
-
-                        <div class="row">
-                    <div class="col-md-4">DATE :<input type="text" class="form-control" id="inputSuccess4" aria-describedby="inputSuccess4Status"></div>
-                    <div class="col-md-4 col-md-offset-4">MODE DE REGLEMENT :<input type="text" class="form-control" id="inputSuccess4" aria-describedby="inputSuccess4Status"></div></div>
+    <form action="" method="post">
+        
+                                
+                    <div class="row">
+                    <div class="col-md-4">DATE :<input type="text" class="form-control input-lg" id="inputSuccess4" aria-describedby="inputSuccess4Status" name="DATE"></div>
+                    <div class="col-md-4 col-md-offset-4">MODE DE REGLEMENT :<input type="text" class="form-control input-lg" id="inputSuccess4" aria-describedby="inputSuccess4Status" name="MODE_DE_REGLEMENT"></div>
                     </div>
-
-                    <form class="form-horizontal">
-                        <div class="control-group form-group-lg">
-                            <label class="control-label" for=
-                            "inputTravaux">TRAVAUX:</label>
-                            <div class="form-group form-group-lg">
-                            <div class="controls">
-                            <input id="inputTravaux" placeholder=
-                                "" type="text">
-                            </div>
-                        </div>
-                        </form>
-
-
+                    
+                            
+                    <div class="row">
+                    <div class="col-md-4">TRAVAUX :<textarea class="form-control input-lg" name="TRAVAUX" rows="2"></textarea></div>
+                    <div class="col-md-4 col-md-offset-4"><button type="button" class="btn btn-success btn-lg" name= "Enregistrer_le_chantier">Enregistrer le chantier</button></div>
+                    </div>
+            
+            
+                    <div class ="row">
+                    <div class="col-md-4">MONTANT :<input type="text" class="form-control input-lg" id="inputSuccess4" aria-describedby="inputSuccess4Status" name="Montant"></div>
+                    </div>
+                    
+                    
+                        <div class ="row">
                         <div class="control-group">
-                            <label class="control-label" for=
-                            "inputMontant">MONTANT:</label>
-                            <div class="controls">
-                                <input id="inputMontant" placeholder=
-                                "" type="text">
+                            <label class="control-group" for=
+                            "inputNote">NOTES :</label>
+                            <textarea class="form-control input-lg" name="NOTES" rows="8"></textarea>    
                             </div>
-                        </div>
-
-                        <div class="control-group">
-                            <label class="control-label" for=
-                            "inputNote">NOTE:</label>
-                            <div class="controls">
-                            <textarea class="form-control input-lg" rows="3"></textarea>    
                             </div>
-                        </div>
-
-
-                        
+                    
+                            </form>
+                                </div>
+            
 
             
+                                        <!--PARTIE PHP  -->
+
+
+
+<?php
+
+if (!empty($_POST))
+
+{
+
+include('assets/templates/tryCatch.php');
+
+    $date_debut = htmlspecialchars($_POST['date de debut']); 
+    $description = htmlspecialchars($_POST['description']);
+    $mode_de_paiment = htmlspecialchars($_POST['mode de paiment']);
+    $facture = htmlspecialchars($_POST['facture']);
+    $photos = htmlspecialchars($_POST['photos']); 
+
+
+$sql_creation_chantier = sprintf("INSERT INTO 'SPH' '.' 'tra_travaux'('tra_date_debut', 'tra_description', 'tra_mode_paiment', 'tra_facture', 'tra_photos') 
+VALUES ('%s', '%s', '%s', '%s' , '%s')" ,
+    $date_debut, $description, $mode_de_paiment, $facture, $photos);
+
+    try
+    {
+    
+        $bdd->query($sql_creation_chantier);
+    
+    }
+    
+    
+    catch (Exception $e)
+    {
+
+        die('erreur: ' .$e->getMessage());
+
+    };  
+
+
+    header("Localisation: ?p=creation_chantier");
+    
+    };
+    
+    
+    
+    
+    
