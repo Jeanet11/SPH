7cc8c2a35d7a96ae36fdcaff1095d9c07b077a67
Jeanne <jeanne.tiramani@hotmail.fr>
2017-10-12 16:24:39 +0200

    recherche client ok

---
+++ pages/recherche_client.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!empty($_POST)){
+    
+    include('assets/templates/tryCatch.php');
+
+    $nom = htmlspecialchars($_POST["nom"]);
+}
+
+    $reponse=$bdd->query(sprintf("SELECT * FROM cli_client  WHERE cli_nom LIKE '%%%s%%' ", $nom));
+  ?>
+<div class="container">
+
+
+<?php
+	
+
+    while($donnees = $reponse->fetch())
+			{
+
+
+?>
+
+		<a  href="?p=fiche_client&id=<?= $donnees['cli_oid']  ?>">
+	<ul class="list-inline row">
+
+			<li class="list-group-item col-md-offset-1 col-md-1 col-xs-1"> <?=$donnees['cli_oid']; ?></li>
+			
+			<li class="list-group-item col-md-3 col-xs-5"> <?=$donnees['cli_nom']; ?></li>
+			
+			<li class="list-group-item col-md-2 col-xs-6"> <?=$donnees['cli_prenom']; ?></li>
+			<li class="list-group-item col-md-1 col-xs-6"> <?=$donnees['cli_cp']; ?></li>
+			<li class="list-group-item col-md-3 col-xs-6"> <?=$donnees['cli_ville']; ?></li>
+		</ul>
+</a>	
+
+
+<?php
+
+	}
+$reponse->closeCursor();
+	?>
+	
