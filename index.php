6bb9233182d85141d1c4eac0cf7196846226b2a3
chrisjuin <c.juin.cj@gmail.com>
2017-10-12 15:45:14 +0200

    modification index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ ob_start();
 if($p === 'connexion'){
     include('./pages/connexion.php');
 }
-<<<<<<< HEAD
+
 if($p === 'creation_chantier'){
     include('./pages/creation_chantier.php');
 }
-=======
+
 if($p === 'liste_chantier'){
     include('./pages/liste_chantier.php');
 }
@@ -36,7 +36,7 @@ if($p === 'deconnexion'){
 	include('./pages/deconnexion.php');
 }
 
->>>>>>> f79184ef550b72f56456013006982673e30dcc92
+
 $content = ob_get_clean();
 include('assets/templates/default.php');
 ?>
