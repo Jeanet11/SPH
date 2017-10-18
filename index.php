9b89cca8af02d5c71b99298062d30e516b5b81de
Jim29800 <jbouvier1173@gmail.com>
2017-10-18 16:35:51 +0200

    doublon supprimé

--- index.php
+++ index.php
@@ -77,10 +77,6 @@ if($p === 'update_mdp') {
 	include('./pages/update_mdp.php');
 }
 
-if($p === 'update_mdp') {
-	include('./pages/update_mdp.php');
-}
-
 $content = ob_get_clean();
 include('assets/templates/default.php');
 ?>
