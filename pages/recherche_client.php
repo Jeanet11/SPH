beba61905f6f5b5c2cb8ba30cec652d37ccc65a2
Jim29800 <jbouvier1173@gmail.com>
2017-10-19 11:11:07 +0200

    ajout message en cas de recherche infructueuse et refactoring

--- pages/recherche_client.php
+++ pages/recherche_client.php
@@ -1,46 +1,34 @@
 <?php
 //verifie l'identification
-if (empty($_SESSION['uti_pseudo'])){
-    header("Location: ?p=connexion");
+if (empty($_SESSION['uti_pseudo'])) {
+	header("Location: ?p=connexion");
 };
-if (!empty($_POST)){
-    
-    include('assets/templates/tryCatch.php');
+if (!empty($_POST)) {
 
-    $nom = htmlspecialchars($_POST["recherche"]);
+	include ('assets/templates/tryCatch.php');
+
+	$nom = htmlspecialchars($_POST["recherche"]);
 }
 
-    $reponse=$bdd->query(sprintf("SELECT * FROM cli_client  WHERE cli_nom LIKE '%%%s%%' ", $nom));
-  ?>
-<div class="container">
-
-
-<?php
-	
-
-    while($donnees = $reponse->fetch())
-			{
-
-
+$reponse = $bdd->query(sprintf("SELECT * FROM cli_client  WHERE cli_nom LIKE '%%%s%%' ", $nom))->fetchAll();
 ?>
-
-		<a  href="?p=fiche_client&id=<?= $donnees['cli_oid']  ?>">
-	<ul class="list-inline row">
-
-			<li class="list-group-item col-md-offset-1 col-md-1 col-xs-1"> <?=$donnees['cli_oid']; ?></li>
-			
-			<li class="list-group-item col-md-3 col-xs-5"> <?=$donnees['cli_nom']; ?></li>
-			
-			<li class="list-group-item col-md-2 col-xs-6"> <?=$donnees['cli_prenom']; ?></li>
-			<li class="list-group-item col-md-1 col-xs-6"> <?=$donnees['cli_cp']; ?></li>
-			<li class="list-group-item col-md-3 col-xs-6"> <?=$donnees['cli_ville']; ?></li>
-		</ul>
-</a>	
-
-
+<div class="container">
 <?php
-
-	}
-$reponse->closeCursor();
-	?>
+if ((!empty($reponse))):
+	foreach ($reponse as $donnees) :
+		?>
+		<a  href="?p=fiche_client&id=<?= $donnees['cli_oid'] ?>">
+			<ul class="list-inline row">
+					<li class="list-group-item col-md-offset-1 col-md-1 col-xs-1"> <?= $donnees['cli_oid']; ?></li>
+				<li class="list-group-item col-md-3 col-xs-5"> <?= $donnees['cli_nom']; ?></li>
+				<li class="list-group-item col-md-2 col-xs-6"> <?= $donnees['cli_prenom']; ?></li>
+				<li class="list-group-item col-md-1 col-xs-6"> <?= $donnees['cli_cp']; ?></li>
+				<li class="list-group-item col-md-3 col-xs-6"> <?= $donnees['cli_ville']; ?></li>
+			</ul>
+		</a>	
+<?php
+	endforeach;
+else : echo '<h2 class="text-center">Aucune correspondance trouvée.</h2>';
+endif;
+?>
 	
